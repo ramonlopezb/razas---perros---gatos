36aef51b8ce248b12d2a12a9f67a06d482ced429
ramonlopezb <ramonlb54@gmail.com>
2023-11-14 08:31:23 -0300

    utilizando API key
    se utiliza key para API dog

--- src/pet-shop-website-template/js/pmain.js
+++ src/pet-shop-website-template/js/pmain.js
@@ -1,4 +1,3 @@
-console.log("hello world");
 let perro = document.getElementById("perros");
 perro.innerHTML = "Perros";
 let razasPerro = document.getElementById("razas de Perros")
@@ -6,14 +5,29 @@ razasPerro.innerHTML = "Existen más de 100 razas de perros en todo el mundo, al
 let h4 = document.getElementById("parrafoPerros");
 h4.innerHTML = "Entre las más conocidas se encuentran.Affenpinscher. Bulldog. Chihuahua.Doberman.Labrador. Espaniel.Retriever.Poodle. Pitbull. Pinscher."
 
-const URL = 'https://dog.ceo/api/breeds/list/all';
-const URL_IMAGENES= 'https://dog.ceo/api/breeds/image/random/10';
+const URL = 'https://api-ninjas.com/api?&O4TWrUym6Vu5qX5MdTtaoQ==qzxgRdqGOBrv3VJy';
+//const URL_IMAGENES= 'https://dog.ceo/api/breeds/image/random/10';
 
+var name = 'golden retriever'
+
+async function fetchdata(breedName){
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/dogs?name=' + breedName,
+        headers: { 'X-Api-Key': 'O4TWrUym6Vu5qX5MdTtaoQ==qzxgRdqGOBrv3VJy'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
 
 async function razaPerro(){
-    const res = await fetch(URL);
-    const data = await res.json();
-    console.log(data);
+    
+    console.log(fetchdata("golden retriever"));
     
 }
 razaPerro();
@@ -21,7 +35,7 @@ razaPerro();
 async function imagenPerro(){
     const res = await fetch(URL_IMAGENES);
     const data =await res.json();
-    console.log(data);
+   // console.log(data);
     let imagenesPerros = data["message"];
     console.log(imagenesPerros);
     let imgPerros = document.getElementById("imagenGeneralPerros");
